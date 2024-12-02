c1997efdcde1942897c22ea97505014762928271
hypeAlive <nicolasfr002@gmail.com>
2024-12-02 13:54:42 +0100

    connect to wifi

--- src/main.c
+++ src/main.c
@@ -1,12 +1,96 @@
+#include <lwip/ip4_addr.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_system.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "esp_http_client.h"
 
+#define WIFI_SSID "HSFD-M2M"
+#define WIFI_PASS "IoTwithMachine2MachineOnly!"
 
-void app_main() {
-    while(1) {
-        printf("Failed to read Lux value\n");
-        vTaskDelay(pdMS_TO_TICKS(500));
+static const char *TAG = "wifi_station";
+
+static void http_get_task(void *pvParameters) {
+    esp_http_client_config_t config = {
+            .method = HTTP_METHOD_GET,
+            .auth_type = HTTP_AUTH_TYPE_NONE,
+            .url = "http://api.chucknorris.io/jokes/random",
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %lld",
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+        char buffer[512];
+        int content_length = esp_http_client_read(client, buffer, sizeof(buffer) - 1);
+        if (content_length >= 0) {
+            buffer[content_length] = 0; // Null-terminate the buffer
+            ESP_LOGI(TAG, "HTTP GET Response:\n%s", buffer);
+        } else {
+            ESP_LOGE(TAG, "Failed to read response");
+        }
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
     }
+    esp_http_client_cleanup(client);
+    vTaskDelete(NULL);
+}
+
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_err_t ok = esp_wifi_connect();
+        ESP_LOGI(TAG, "wifi sta start %s", ok == ESP_OK ? "success" : "failed");
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+        ESP_LOGI(TAG, "retry to connect to the AP");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+        ESP_LOGI(TAG, "got ip:%s", ip4addr_ntoa(&event->ip_info.ip));
+        xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+    }
+}
+
+void wifi_init_sta(void) {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id);
+    esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip);
+
+    wifi_config_t wifi_config = {
+            .sta = {
+                    .ssid = WIFI_SSID,
+                    .password = WIFI_PASS,
+            },
+    };
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+    esp_wifi_start();
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+}
+
+void app_main(void) {
+    vTaskDelay(pdMS_TO_TICKS(2000));
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
 }
